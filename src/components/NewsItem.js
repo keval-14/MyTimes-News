0aa3b7194066a0ef8b69fd5f59512188c6004079
Keval Mangukiya <work.kevalpatel@gmail.com>
2022-07-01 15:10:04 +0530

    code updated

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,11 +8,11 @@ export class NewsItem extends Component {
     return (
         <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-                <img src={imgurl}className="card-img-top" alt="..."/>
+                <img src={imgurl}className="card-img-top" alt="..." height={190} width={286}/>
                 <div className="card-body">
                 <h5 className="card-title">{title}...</h5>
                 <p className="card-text">{description}...</p>
-                <a href={newsurl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+                <a rel="noreferrer" href={newsurl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                 </div>
       </div>
       </div>
